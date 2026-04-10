de59574a8815bbf9391a1b4224b719cb7d822237
abir <abir196600@gmail.com>
2026-04-10 11:37:02 +0600

    Created activity schema

---
+++ server/models/Activity.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+
+const activitySchema = new mongoose.Schema(
+  {
+    u: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      index: true,
+    },
+
+    t: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    c: {
+      type: Number,
+      required: true,
+      enum: [0, 1, 2], // focused, maintenance, leisure
+      index: true,
+    },
+
+    d: {
+      type: Number,
+      required: true, // duration in minutes
+      min: 1,
+    },
+
+    dt: {
+      type: Number, // YYYYMMDD
+      required: true,
+      index: true,
+    },
+  },
+  {
+    timestamps: { createdAt: true, updatedAt: false },
+  },
+);
+
+// Compound Indexes (important for your queries)
+activitySchema.index({ u: 1, dt: 1 });
+activitySchema.index({ u: 1, c: 1 });
+
+export const Activity = mongoose.model("Activity", activitySchema);
